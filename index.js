e7f808e9604cc0bc325a9d73a5ec20ef263ead30
J-Santoyo <joanasantoyo030@gmail.com>
2022-01-24 12:26:26 -0700

    added prompts for usage installation and contributions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const promptUser = () => {
         {
             type: "input",
             name: "description",
-            message: "Please add a brief description of your project"
+            message: "Please add a brief description of your project."
         },
         {
             type: "list",
@@ -19,6 +19,21 @@ const promptUser = () => {
             message: "Please pick a license",
             choices: ["MIT", "Boost Software License 1.0", "Unlicense"]
 
-        }, 
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please add usage instructions."
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the instructions for installation?"
+        },
+        {
+            type: "input",
+            name: "contributions",
+            message: "Please enter in any contribution guidelines:"
+        },    
     ])
 }
