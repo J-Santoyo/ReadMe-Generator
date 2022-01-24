269bda879a53ab68412354ca44e8780e3317f6c7
J-Santoyo <joanasantoyo030@gmail.com>
2022-01-24 12:06:41 -0700

    added prompts for title descrip and license

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please add a brief description of your project"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please pick a license",
+            choices: ["MIT", "Boost Software License 1.0", "Unlicense"]
+
+        }, 
+    ])
+}
